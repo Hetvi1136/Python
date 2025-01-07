251c9c93766238d93ed697e8ce5622fabb664b0d
Hetvi1136 <hetvi3545@gmail.com>
2025-01-07 15:39:30 +0530

    Add files via upload

---
+++ hours to minutes.py
@@ -0,0 +1,10 @@
+#//convert hours into minutes
+hours=int(input("enter the hours"))
+minutes= hours*60
+print(minutes)
+
+#//convert minutes into hours
+minutes=int(input("enter the minutes"))
+hours=minutes/60
+print(hours)
+
