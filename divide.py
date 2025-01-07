59568c9018fde105425e1ba9bcff4b33dc553ba4
Hetvi1136 <hetvi3545@gmail.com>
2025-01-07 15:27:21 +0530

    Add files via upload

---
+++ divide.py
@@ -0,0 +1,10 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 13-11
+2
+>>> 13*11
+143
+>>> 12/3
+4.0
+>>> 8+9
+17
