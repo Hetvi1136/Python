0011e9670fa5ecc23eb2774b045bde29216a3315
Hetvi1136 <hetvi3545@gmail.com>
2025-01-19 22:00:04 +0530

    Create celcius and fahrenhit .py

---
+++ celcius and fahrenhit .py
@@ -0,0 +1,10 @@
+
+#//celcius into fahrenhit
+celcius  =int(input("enter the  celcius  "))
+fahrenhit=(9/5*celcius)+32
+print(fahrenhit)
+
+#// fahrenhit into celcius 
+fahrenhit =int(input("enter the  fahrenhit "))
+celcius =5/9*(fahrenhit-32)
+print(celcius )
