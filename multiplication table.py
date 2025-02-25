4503188d4baf08b726b85facb14595aefd641661
Hetvi1136 <hetvi3545@gmail.com>
2025-02-25 13:28:51 +0530

    Add files via upload

--- multiplication table.py
+++ multiplication table.py
@@ -1,5 +1,3 @@
-a = int(input("enter a number to write multiplication num"))
-i=0
-while i<10:
-    i+=1
-    print(a * i)
+n=int(input('enter a number: '))
+for i in range(0,n+1):
+    print(i,'*',n,'=',i*n)
